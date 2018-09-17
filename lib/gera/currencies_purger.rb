a6c053b8e48fad1ef194db2d7a70ae165688b4c8
Danil Pismennyy <danil@damacbook.local>
2018-09-17 23:34:03 +0300

    minor

--- lib/gera/currencies_purger.rb
+++ lib/gera/currencies_purger.rb
@@ -1,26 +1,28 @@
-module CurrenciesPurger
-  def self.purge_all(env)
-    raise unless env == Rails.env
+module GERA
+  module CurrenciesPurger
+    def self.purge_all(env)
+      raise unless env == Rails.env
 
-    if Rails.env.prodiction?
-      puts 'Disable all sidekiqs'
-      Sidekiq::Cron::Job.all.each(&:disable!)
-      sleep 2
-    end
+      if Rails.env.prodiction?
+        puts 'Disable all sidekiqs'
+        Sidekiq::Cron::Job.all.each(&:disable!)
+        sleep 2
+      end
 
-    DirectionRateSnapshot.batch_purge if DirectionRateSnapshot.table_exists?
-    DirectionRate.batch_purge
+      DirectionRateSnapshot.batch_purge if DirectionRateSnapshot.table_exists?
+      DirectionRate.batch_purge
 
-    ExternalRate.batch_purge
-    ExternalRateSnapshot.batch_purge
+      ExternalRate.batch_purge
+      ExternalRateSnapshot.batch_purge
 
-    CurrencyRate.batch_purge
-    RateSource.update_all actual_snapshot_id: nil
-    CurrencyRateSnapshot.batch_purge
+      CurrencyRate.batch_purge
+      RateSource.update_all actual_snapshot_id: nil
+      CurrencyRateSnapshot.batch_purge
 
-    if Rails.env.prodiction?
-      puts 'Enable all sidekiqs'
-      Sidekiq::Cron::Job.all.each(&:enable!)
+      if Rails.env.prodiction?
+        puts 'Enable all sidekiqs'
+        Sidekiq::Cron::Job.all.each(&:enable!)
+      end
     end
   end
 end
