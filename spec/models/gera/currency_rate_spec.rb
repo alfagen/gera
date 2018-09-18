f7b648ae1e82cd69606f3eb31a7d666a772702ab
Danil Pismenny <danil@brandymint.ru>
2018-09-18 23:28:41 +0300

    добавил spec в docs

--- spec/models/gera/currency_rate_spec.rb
+++ spec/models/gera/currency_rate_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 
 describe Gera::CurrencyRate do
   subject { create :currency_rate }
-  it { expect(subject).to be_persisted }
+  it 'persisted' do
+    expect(subject).to be_persisted
+  end
 end
--- spec/models/gera/direction_rate_spec.rb
+++ spec/models/gera/direction_rate_spec.rb
@@ -5,7 +5,7 @@ describe Gera::DirectionRate do
     allow( Gera::DirectionsRatesWorker ).to receive :perform_async
   end
   subject { create :direction_rate }
-  it do
+  it 'persosted' do
     expect(subject).to be_persisted
   end
 end
