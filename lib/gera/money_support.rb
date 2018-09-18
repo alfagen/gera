0be5f85ae6c3cec448f20b388e71d84b7cdd0403
Danil Pismenny <danil@brandymint.ru>
2018-09-18 21:11:03 +0300

    Выделил  MoneySupport.init

--- lib/gera/money_support.rb
+++ lib/gera/money_support.rb
@@ -1,5 +1,21 @@
 module Gera
   module MoneySupport
+    def self.init
+      # Убираем все валюты
+      Money::Currency.all.each do |cur|
+        Money::Currency.unregister cur.id.to_s
+      end
+
+      Psych.load( File.read CURRENCIES_PATH ).each { |key, cur| Money::Currency.register cur.symbolize_keys }
+
+      # Создают константы-валюты, типа RUB, USD и тп
+      Money::Currency.all.each do |cur|
+        Object.const_set cur.iso_code, cur
+      end
+
+      # Gera::Hooks.init
+    end
+
     module CurrencyExtend
       # TODO Вынести в app
       attr_reader :local_id
