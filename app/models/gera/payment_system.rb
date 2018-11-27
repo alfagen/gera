e86a6bfc67b597333fe0edd93dd77727115f6df2
Alexey Naumov <alexsnaumov@gmail.com>
2018-11-27 15:13:40 +0300

    расчет итоговой суммы с учетом комиссий в платежной системе

--- app/models/gera/payment_system.rb
+++ app/models/gera/payment_system.rb
@@ -3,12 +3,15 @@ require 'archivable'
 module Gera
   class PaymentSystem < ApplicationRecord
     include ::Archivable
+    include Gera::Mathematic
 
     self.table_name = :cms_paymant_system
 
     scope :ordered, -> { order :priority }
     scope :enabled,  -> { where 'income_enabled>0 or outcome_enabled>0' }
     scope :disabled,  -> { where income_enabled: false, outcome_enabled: false }
+
+    enum total_computation_method: %i(regular_fee reverse_fee)
 
     validates :name, presence: true, uniqueness: true
     validates :currency, presence: true
@@ -40,12 +43,39 @@ module Gera
       name
     end
 
-    # TODO: надо пособирать такие условия и может для каждой платежки сделать json с настройками
-    def income_fee_percents
-      income_fee.percents
+    def total_with_fee(money)
+      calculate_total(money: money, fee: transfer_fee)
+    end
+
+    def unverified_total_with_fee(money)
+      calculate_total(money: money, fee: unverified_transfer_fee)
     end
 
     private
+
+    def calculate_total(money: , fee:)
+      if fee.computation_method == 'regular_fee'
+        calculate_total_using_regular_comission(money, fee.amount)
+      elsif fee.computation_method == 'reverse_fee'
+        calculate_total_using_reverse_comission(money, fee.amount)
+      else
+        raise NotImplementedError.new("Нет расчета для #{fee.computation_method}")
+      end
+    end
+
+    def transfer_fee
+      OpenStruct.new(
+        amount: income_fee,
+        computation_method: total_computation_method
+      ).freeze
+    end
+
+    def unverified_transfer_fee
+      OpenStruct.new(
+        amount: unverified_income_fee,
+        computation_method: total_computation_method
+      ).freeze
+    end
 
     def create_exchange_rates
       PaymentSystem.pluck(:id).each do |foreign_id|
