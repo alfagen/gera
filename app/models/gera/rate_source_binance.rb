adfa692e0b532414c242afddc5fc28f1b6a23e7c
Roman <roman0tershak@gmail.com>
2021-10-12 12:32:26 +0300

    Добавляем новую крипту: Stellar, Polkadot (#8)

--- app/models/gera/rate_source_binance.rb
+++ app/models/gera/rate_source_binance.rb
@@ -3,7 +3,7 @@
 module Gera
   class RateSourceBinance < RateSource
     def self.supported_currencies
-      %i[BTC BCH DSH ETH ETC LTC XRP XMR ZEC NEO EOS ADA XEM WAVES TRX DOGE BNB].map { |m| Money::Currency.find! m }
+      %i[BTC BCH DSH ETH ETC LTC XRP XMR ZEC NEO EOS ADA XEM WAVES TRX DOGE BNB XLM DOT USDT].map { |m| Money::Currency.find! m }
     end
   end
 end
