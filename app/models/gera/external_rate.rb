6883989b5f6d6bb3e7878bb5275cd0d434b89dbd
Roman Tershak <roman0tershak@gmail.com>
2022-06-10 16:11:41 +0300

    Fix stage error

--- app/models/gera/external_rate.rb
+++ app/models/gera/external_rate.rb
@@ -10,8 +10,6 @@ module Gera
 
     scope :ordered, -> { order :cur_from, :cur_to }
 
-    scope :with_auto_rates, -> { where(auto_rate_enabled: true) }
-
     before_validation do
       self.source ||= snapshot.try :rate_source
     end
@@ -30,31 +28,7 @@ module Gera
       as_json(only: %i[id cur_from cur_to rate_value source_id created_at])
     end
 
-    def final_rate_percents
-      auto_rate_enabled? ? auto_rate_value_by_reserve : comission_percents
-    end
-
     private
-
-    def auto_rate_value_by_reserve
-      ((auto_rate_by_reserve_from_boundary + auto_rate_by_reserve_to_boundary) / 2.0).round(2)
-    end
-
-    def auto_rate_by_reserve_from_boundary
-      min_checkpoint = payment_system_from.auto_rate_settings.find_by(direction: :income)&.checkpoint
-      max_checkpoint = payment_system_to.auto_rate_settings.find_by(direction: :outcome)&.checkpoint
-      return 0.0 if min_checkpoint.nil? || max_checkpoint.nil?
-
-      ((min_checkpoint.min_boundary + max_checkpoint.min_boundary) / 2.0).round(2)
-    end
-
-    def auto_rate_by_reserve_to_boundary
-      min_checkpoint = payment_system_from.auto_rate_settings.find_by(direction: :income)&.checkpoint
-      max_checkpoint = payment_system_to.auto_rate_settings.find_by(direction: :outcome)&.checkpoint
-      return 0.0 if min_checkpoint.nil? || max_checkpoint.nil?
-
-      ((min_checkpoint.max_boundary + max_checkpoint.max_boundary) / 2.0).round(2)
-    end
 
     def upcase_currencies
       self.cur_from = cur_from.to_s.upcase if cur_from.present?
