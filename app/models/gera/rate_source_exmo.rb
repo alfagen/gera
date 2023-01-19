d068e4cc2ea1c91c74c3086c37b298aa4eaa623c
Roman Tershak <roman0tershak@gmail.com>
2023-01-19 21:21:52 +0200

    Добавляем монету TON

--- app/models/gera/rate_source_exmo.rb
+++ app/models/gera/rate_source_exmo.rb
@@ -3,7 +3,7 @@
 module Gera
   class RateSourceEXMO < RateSource
     def self.supported_currencies
-      %i[BTC BCH DSH ETH ETC LTC XRP XMR USD RUB ZEC EUR USDT NEO EOS ADA XEM WAVES TRX DOGE].map { |m| Money::Currency.find! m }
+      %i[BTC BCH DSH ETH ETC LTC XRP XMR USD RUB ZEC EUR USDT NEO EOS ADA XEM WAVES TRX DOGE TON].map { |m| Money::Currency.find! m }
     end
   end
 end
