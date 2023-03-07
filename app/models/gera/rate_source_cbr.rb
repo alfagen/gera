5e0fc1d732cb23016c61446c7e0c9ee6d4d37842
Roman Tershak <roman0tershak@gmail.com>
2023-03-07 16:27:11 +0200

    Добавляем валюту: Узбекские сумы (UZS)

--- app/models/gera/rate_source_cbr.rb
+++ app/models/gera/rate_source_cbr.rb
@@ -3,11 +3,11 @@
 module Gera
   class RateSourceCBR < RateSource
     def self.supported_currencies
-      %i[RUB KZT USD EUR UAH].map { |m| Money::Currency.find! m }
+      %i[RUB KZT USD EUR UAH UZS].map { |m| Money::Currency.find! m }
     end
 
     def self.available_pairs
-      ['KZT/RUB', 'USD/RUB', 'EUR/RUB', 'UAH/RUB'].map { |cp| Gera::CurrencyPair.new cp }.freeze
+      ['KZT/RUB', 'USD/RUB', 'EUR/RUB', 'UAH/RUB', 'UZS/RUB'].map { |cp| Gera::CurrencyPair.new cp }.freeze
     end
   end
 end
