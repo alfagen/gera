36e38abd91f12981e117251e9907c4a522fd2139
Roman Tershak <roman0tershak@gmail.com>
2024-06-24 18:37:38 +0300

    Временно удалил WAVES, XEM

--- app/models/gera/rate_source_exmo.rb
+++ app/models/gera/rate_source_exmo.rb
@@ -3,7 +3,7 @@
 module Gera
   class RateSourceExmo < RateSource
     def self.supported_currencies
-      %i[BTC BCH DSH ETH ETC LTC XRP XMR USD RUB ZEC EUR USDT NEO EOS ADA XEM WAVES TRX DOGE TON].map { |m| Money::Currency.find! m }
+      %i[BTC BCH DSH ETH ETC LTC XRP XMR USD RUB ZEC EUR USDT NEO EOS ADA TRX DOGE TON].map { |m| Money::Currency.find! m }
     end
   end
 end
