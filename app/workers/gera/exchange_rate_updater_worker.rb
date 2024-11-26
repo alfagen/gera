f9a6b31b3c3e446899157c5e62d05b2dd9ccf419
Roman Tershak <roman0tershak@gmail.com>
2024-11-26 18:03:54 +0200

    f

--- app/workers/gera/exchange_rate_updater_worker.rb
+++ app/workers/gera/exchange_rate_updater_worker.rb
@@ -11,7 +11,7 @@ module Gera
 
     def perform(exchange_rate_id, attributes, timestamp = nil)
       ExchangeRate.find(exchange_rate_id).update(attributes)
-      LOGGER.info("#manual#: after update #{exchange_rate.id} - #{timestamp} - #{Time.current.to_i}") unless timestamp.nil?
+      LOGGER.info("#manual#: after update #{exchange_rate_id} - #{timestamp} - #{Time.current.to_i}") unless timestamp.nil?
     end
   end
 end
