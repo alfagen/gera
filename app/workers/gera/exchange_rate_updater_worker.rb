1a544eb16542d2530fb033826100ecf53003817f
Roman Tershak <roman0tershak@gmail.com>
2024-11-26 17:36:10 +0200

    fix

--- app/workers/gera/exchange_rate_updater_worker.rb
+++ app/workers/gera/exchange_rate_updater_worker.rb
@@ -7,8 +7,10 @@ module Gera
 
     sidekiq_options queue: :exchange_rates
 
-    def perform(exchange_rate_id, attributes)
+    def perform(exchange_rate_id, attributes, timestamp = nil)
+      logger = Logger.new("#{Rails.root}/log/operator_exchange_rates_api.log")
       ExchangeRate.find(exchange_rate_id).update(attributes)
+      logger.info("#manual#: after update #{exchange_rate.id} - #{timestamp} - #{Time.current.to_i}") unless timestamp.nil?
     end
   end
 end
