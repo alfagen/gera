0055ae456c06c58cb6f4099fa6d0276092ece43d
Roman <roman0tershak@gmail.com>
2024-11-27 15:12:39 +0200

    Сбор логов по exchange_rates (#49)

--- app/workers/gera/exchange_rate_updater_worker.rb
+++ app/workers/gera/exchange_rate_updater_worker.rb
@@ -7,8 +7,11 @@ module Gera
 
     sidekiq_options queue: :exchange_rates
 
-    def perform(exchange_rate_id, attributes)
+    LOGGER = Logger.new(File.expand_path("~/admin.kassa.cc/current/log/operator_exchange_rates_api.log"))
+
+    def perform(exchange_rate_id, attributes, timestamp = nil)
       ExchangeRate.find(exchange_rate_id).update(attributes)
+      LOGGER.info("#manual#: after update #{exchange_rate_id} - #{timestamp} - #{Time.current.to_i}") unless timestamp.nil?
     end
   end
 end
