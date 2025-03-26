01b98520a596f928fa50a6a2773b9ef2340730f2
Roman <roman0tershak@gmail.com>
2025-03-26 14:40:15 +0200

    Новый источник курсов: FF (#55)

---
+++ lib/gera/ff_float_fetcher.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Gera
+  class FfFloatFetcher
+    API_URL = 'https://ff.io/rates/float.xml'
+    Error = Class.new StandardError
+
+    def perform
+      rates.each_with_object({}) do |rate, memo|
+        cur_from, cur_to = rate[:from], rate[:to]
+        cur_from = 'BNB' if cur_from == 'BSC'
+        cur_to = 'BNB' if cur_to == 'BSC'
+        next unless supported_currencies.include?(cur_from)
+        next unless supported_currencies.include?(cur_to)
+
+        pair = Gera::CurrencyPair.new(cur_from: cur_from, cur_to: cur_to)
+        reverse_pair = Gera::CurrencyPair.new(cur_from: cur_to, cur_to: cur_from)
+
+        memo[pair] = rate unless memo.key?(reverse_pair)
+      end
+    end
+
+    private
+
+    def rates
+      xml_data = URI.open(API_URL).read
+      doc = Nokogiri::XML(xml_data)
+      res = []
+
+      doc.xpath('//item').each do |item|
+        rate_info = {
+          from: item.at('from')&.text,
+          to: item.at('to')&.text,
+          in: item.at('in')&.text.to_f,
+          out: item.at('out')&.text.to_f,
+          amount: item.at('amount')&.text.to_f,
+          tofee: item.at('tofee')&.text,
+          minamount: item.at('minamount')&.text,
+          maxamount: item.at('maxamount')&.text
+        }
+        res << rate_info
+      end
+
+      res
+    end
+
+    def supported_currencies
+      @supported_currencies ||= RateSourceFfFloat.supported_currencies
+    end
+  end
+end
