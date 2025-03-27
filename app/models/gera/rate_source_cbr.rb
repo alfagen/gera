445cb30d439258dcc2ede37a045f9deda30c9b8d
Roman <roman0tershak@gmail.com>
2025-03-27 13:13:53 +0200

    Новая валюта: Индонезийская Рупия (#56)

--- app/models/gera/rate_source_cbr.rb
+++ app/models/gera/rate_source_cbr.rb
@@ -3,11 +3,11 @@
 module Gera
   class RateSourceCbr < RateSource
     def self.supported_currencies
-      %i[RUB KZT USD EUR UAH UZS AZN BYN TRY THB].map { |m| Money::Currency.find! m }
+      %i[RUB KZT USD EUR UAH UZS AZN BYN TRY THB IDR].map { |m| Money::Currency.find! m }
     end
 
     def self.available_pairs
-      ['KZT/RUB', 'USD/RUB', 'EUR/RUB', 'UAH/RUB', 'UZS/RUB', 'AZN/RUB', 'BYN/RUB', 'TRY/RUB', 'THB/RUB'].map { |cp| Gera::CurrencyPair.new cp }.freeze
+      ['KZT/RUB', 'USD/RUB', 'EUR/RUB', 'UAH/RUB', 'UZS/RUB', 'AZN/RUB', 'BYN/RUB', 'TRY/RUB', 'THB/RUB', 'IDR/RUB'].map { |cp| Gera::CurrencyPair.new cp }.freeze
     end
   end
 end
