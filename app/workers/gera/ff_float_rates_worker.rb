d5841e865664d276443692b378d6897b759b2eec
Roman <roman0tershak@gmail.com>
2025-10-14 11:58:39 +0300

    Оптимизация геры (#59)

--- app/workers/gera/ff_float_rates_worker.rb
+++ app/workers/gera/ff_float_rates_worker.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 module Gera
-  # Import rates from FF (Float)
-  #
   class FfFloatRatesWorker
     include Sidekiq::Worker
     include AutoLogger
-
-    prepend RatesWorker
+    include RatesWorker
 
     private
 
@@ -15,12 +12,12 @@ module Gera
       @rate_source ||= RateSourceFfFloat.get!
     end
 
-    def save_rate(pair, data)
-      create_external_rates pair, data, sell_price: data[:out], buy_price: data[:out]
-    end
-
     def load_rates
       FfFloatFetcher.new.perform
+    end
+
+    def rate_keys
+      { buy: 'out', sell: 'out' }
     end
   end
 end
--- app/workers/gera/garantexio_rates_worker.rb
+++ app/workers/gera/garantexio_rates_worker.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 module Gera
-  # Import rates from Garantexio
-  #
   class GarantexioRatesWorker
     include Sidekiq::Worker
     include AutoLogger
-
-    prepend RatesWorker
+    include RatesWorker
 
     private
 
@@ -15,12 +12,12 @@ module Gera
       @rate_source ||= RateSourceGarantexio.get!
     end
 
-    def save_rate(pair, data)
-      create_external_rates pair, data, sell_price: data['last_price'], buy_price: data['last_price']
-    end
-
     def load_rates
       GarantexioFetcher.new.perform
+    end
+
+    def rate_keys
+      { buy: 'last_price', sell: 'last_price' }
     end
   end
 end
